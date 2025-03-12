cbf493c2110768a33caef19657ee9374bfd98fa6
Maik Basel <maik.basel@squer.io>
2025-03-12 14:06:07 +0100

    style: Format code

--- lib/consuming-stack.ts
+++ lib/consuming-stack.ts
@@ -5,17 +5,17 @@ import {Bucket} from "aws-cdk-lib/aws-s3";
 
 export class ConsumingStack extends Stack {
 
-    constructor(scope: Construct, id: string) {
-        super(scope, id);
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
 
-        const lambdaFunction = new Function(this, 'MyLambda', {
-            runtime: Runtime.NODEJS_18_X,
-            handler: 'index.handler',
-            code: Code.fromInline('exports.handler = async () => {};'),
-        })
+    const lambdaFunction = new Function(this, 'MyLambda', {
+      runtime: Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: Code.fromInline('exports.handler = async () => {};'),
+    })
 
-        // const myBucket = Bucket.fromBucketArn(this, 'MyBucket', 'arn:aws:s3:::MyBucket');
-        const myBucket = Bucket.fromBucketName(this, 'MyBucket', 'MyBucket'); // In case of a bucket we don't even need to specify the ARN. We can reference it by name.
-        myBucket.grantRead(lambdaFunction);
-    }
+    // const myBucket = Bucket.fromBucketArn(this, 'MyBucket', 'arn:aws:s3:::MyBucket');
+    const myBucket = Bucket.fromBucketName(this, 'MyBucket', 'MyBucket'); // In case of a bucket we don't even need to specify the ARN. We can reference it by name.
+    myBucket.grantRead(lambdaFunction);
+  }
 }
